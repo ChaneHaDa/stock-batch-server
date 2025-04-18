bf8949e5164b7ff0a1e1fe7a93cf9ed2b979194f
ChaneHaDa <chane.ha.da@gmail.com>
2025-04-18 21:51:57 +0900

    Initial

---
+++ src/main/java/com/chan/stock_batch_server/StockBatchServerApplication.java
@@ -0,0 +1,13 @@
+package com.chan.stock_batch_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockBatchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockBatchServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chan/stock_batch_server/StockBatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan.stock_batch_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockBatchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
