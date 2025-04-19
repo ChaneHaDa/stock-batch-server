6960168c0cbc0983b445b7dd7ad2220da169df02
ChaneHaDa <chane.ha.da@gmail.com>
2025-04-19 21:56:18 +0900

    feat: Index 월별 집계를 위한 entity 추가

---
+++ src/main/java/com/chan/stock_batch_server/model/CalcIndexPrice.java
@@ -0,0 +1,30 @@
+package com.chan.stock_batch_server.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CalcIndexPrice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer price;
+    private float monthlyRor;
+    private LocalDate baseDate;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "index_info_id")
+    private IndexInfo indexInfo;
+}
